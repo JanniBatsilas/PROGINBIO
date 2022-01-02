06ba8908b9defcae7a1586fa7c96702d8fefd42f
Janni BAtsilas <janni.batsilas@stud.unibas.ch>
2022-01-02 12:39:02 +0100

    Add STAR commands and one grep command

--- run_me.sh
+++ run_me.sh
@@ -3,3 +3,14 @@
 python src/execute_tasks.py
 flake8 src/
 coverage run --source src/ -m pytest && coverage report -m
+
+mkdir gen1
+STAR --runThreadN 4 --runMode genomeGenerate --genomeDir gen1 --genomeFastaFiles Mus_musculus.GRCm38.dna_rm.chr19.fa --sjdbGTFfile Mus_musculus.GRCm38.88.chr19.gtf --genomeSAindexNbases 11
+STAR --runThreadN 4 --genomeDir gen1 --readFilesIn reads.mate_1.fq reads.mate_2.fq
+
+grep "^@" -v Aligned.out.sam | grep "^$" -v | wc -l
+
+
+
+
+
